9458c0710937114f10aebc8a4cb2691e8f9cdc33
HurasAdam <huradam@wp.pl>
2023-12-31 05:47:00 +0100

    Feat: Add dummy Data

---
+++ React_Dashboard/src/dummyData/topSeller.js
@@ -0,0 +1,58 @@
+import Seller_one from "../img/seller1.jpg";
+import Seller_two from "../img/seller2.jpg";
+import Seller_three from "../img/seller3.jpg";
+import Seller_four from "../img/seller4.jpg";
+import Seller_five from "../img/seller5.jpg";
+
+const topSeller = [
+  {
+    id: 1,
+    name: "Jessica Swiss",
+    userName: "@swiss",
+    imgSrc: Seller_one,
+  },
+  {
+    id: 2,
+    name: "Joseph Zane",
+    userName: "@joseph",
+    imgSrc: Seller_two,
+  },
+  {
+    id: 3,
+    name: "Andrew Jordan",
+    userName: "@andrew",
+    imgSrc: Seller_three,
+  },
+  {
+    id: 4,
+    name: "Catrine Jones",
+    userName: "@catrine",
+    imgSrc: Seller_four,
+  },
+  {
+    id: 5,
+    name: "Michael Cooper",
+    userName: "@michael",
+    imgSrc: Seller_five,
+  },
+  {
+    id: 6,
+    name: "Jessica Swiss",
+    userName: "@swiss",
+    imgSrc: Seller_one,
+  },
+  {
+    id: 6,
+    name: "Jessica Swiss",
+    userName: "@swiss",
+    imgSrc: Seller_one,
+  },
+  {
+    id: 6,
+    name: "Jessica Swiss",
+    userName: "@swiss",
+    imgSrc: Seller_one,
+  },
+];
+
+export default topSeller;
